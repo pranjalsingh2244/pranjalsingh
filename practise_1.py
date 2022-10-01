6154eec48c4785022b4c74cc3f301c7448d3c5a5
PRANJAL-2244\hp <ss8717935@gmail.com>
2022-10-01 16:49:15 +0530

    good

---
+++ practise_1.py
@@ -0,0 +1,9 @@
+#create two variables (x and y) and assign values to then
+
+#create a third variables(sum) and add x and y
+
+#print the sum
+x = input("enter first number : ")
+y = input("enter second number 2: ")
+sum = int(firstnumber)+int(secondnumber)
+print("the answer is :",sum)
