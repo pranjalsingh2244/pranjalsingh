2c41488573712f4fb073981f104475a5e4371793
PRANJAL-2244\hp <ss8717935@gmail.com>
2022-10-08 21:56:26 +0530

    PRANJAL.py

---
+++ PRANJAL.py
@@ -0,0 +1,16 @@
+
+
+if 5 > 2:
+    print("this is correct")
+    print("this is correct")
+    print("this is correct")
+if 10 > 3:
+    print("this is correct")
+print("hello world")
+a=1
+b=6.5
+print(type(a))
+print(type(b))
+a=5
+b="class"
+print(a+b)
