223a76596fefc8f2415fbd47901297a4ac320acc
PRANJAL-2244\hp <ss8717935@gmail.com>
2022-10-15 19:53:28 +0530

    problem_strings.py

---
+++ problem_strings.py
@@ -0,0 +1,12 @@
+#take astring of length 5 [index 0-4]
+#print first char
+#print middle char
+#print last char
+
+import string
+
+
+string ="string"
+print(string[0])
+print(string[5//2])
+print(string[4])
--- stringslicing.py
+++ stringslicing.py
@@ -1,5 +1,2 @@
 a = "Hello world"
-print(a[0:])
-print(a[8])
-print(a[9])
-print(a.upper())
+print(a.replace("Hello","hey"))
