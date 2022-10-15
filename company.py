3373fda2b7d995580b6bedf53529cd678af765c4
PRANJAL-2244\hp <ss8717935@gmail.com>
2022-10-15 12:40:43 +0530

    hello pranjalsingh

---
+++ company.py
@@ -0,0 +1,20 @@
+#company will give bonus base on following criteria
+
+#time spent in company     bonus
+#   greater than 10 years      10%
+
+#more than 6 and less 
+#       than 10                        8%
+
+#less than 6                           5%
+
+#ask the salary and time spent from user
+#print the net bonus amount accordingly
+
+salary =int (input("what is yor salary"))
+time =int (input("how much time have you spend on this company"))
+
+if time<10:
+    print("you are eligible for 10%of the bonus and your net amount will be" ,salary+(10/100)*salary)
+elif time>6 and time <10:
+    print(if you have spent )
